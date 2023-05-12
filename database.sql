8007f63c3b3d0cc7d0a8419ec444a66427e9ff4f
Gabriel Calil <103449368+Calil05@users.noreply.github.com>
2023-05-12 14:42:28 -0300

    feat: add SQL scripts

---
+++ database.sql
@@ -0,0 +1,168 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: localhost    Database: feedback
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `empresas`
+--
+
+DROP TABLE IF EXISTS `empresas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `empresas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `razao_social` varchar(80) NOT NULL,
+  `nome_fantasia` varchar(80) NOT NULL,
+  `cnpj` varchar(11) NOT NULL,
+  `atividade` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `telefone` varchar(18) NOT NULL,
+  `login` varchar(45) NOT NULL,
+  `senha` varchar(20) NOT NULL,
+  `logo` varchar(400) NOT NULL,
+  `id_endereco` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_empresas_endereco` (`id_endereco`),
+  CONSTRAINT `fk_empresas_endereco` FOREIGN KEY (`id_endereco`) REFERENCES `endereco` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `endereco`
+--
+
+DROP TABLE IF EXISTS `endereco`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `endereco` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `tipo` varchar(30) NOT NULL,
+  `logradouro` varchar(120) NOT NULL,
+  `numero` int NOT NULL,
+  `cep` varchar(8) NOT NULL,
+  `cidade` varchar(45) NOT NULL,
+  `estado` varchar(2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `enquete`
+--
+
+DROP TABLE IF EXISTS `enquete`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `enquete` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `numero` int NOT NULL,
+  `descricao` varchar(120) NOT NULL,
+  `id_empresa` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_enquete_empresa` (`id_empresa`),
+  CONSTRAINT `fk_enquete_empresa` FOREIGN KEY (`id_empresa`) REFERENCES `empresas` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `perguntas`
+--
+
+DROP TABLE IF EXISTS `perguntas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `perguntas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `numero` int NOT NULL,
+  `descricao` varchar(120) NOT NULL,
+  `id_enquete` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_pergunta_enquete` (`id_enquete`),
+  CONSTRAINT `fk_pergunta_enquete` FOREIGN KEY (`id_enquete`) REFERENCES `enquete` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `pessoas`
+--
+
+DROP TABLE IF EXISTS `pessoas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pessoas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) NOT NULL,
+  `sobrenome` varchar(45) NOT NULL,
+  `cpf` varchar(11) NOT NULL,
+  `data_nascimento` date NOT NULL,
+  `telefone` varchar(18) NOT NULL,
+  `login` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `senha` varchar(20) NOT NULL,
+  `id_endereco` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_pessoas_endereco` (`id_endereco`),
+  CONSTRAINT `fk_pessoas_endereco` FOREIGN KEY (`id_endereco`) REFERENCES `endereco` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `respostas`
+--
+
+DROP TABLE IF EXISTS `respostas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `respostas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `numero` int NOT NULL,
+  `descricao` varchar(120) NOT NULL,
+  `id_pergunta` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_resposta_pergunta` (`id_pergunta`),
+  CONSTRAINT `fk_resposta_pergunta` FOREIGN KEY (`id_pergunta`) REFERENCES `perguntas` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `resultados`
+--
+
+DROP TABLE IF EXISTS `resultados`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `resultados` (
+  `id` int NOT NULL,
+  `id_resposta` int NOT NULL,
+  `id_pessoa` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_resultado_resposta` (`id_resposta`),
+  KEY `fk_resultado_pessoa` (`id_pessoa`),
+  CONSTRAINT `fk_resultado_pessoa` FOREIGN KEY (`id_pessoa`) REFERENCES `pessoas` (`id`),
+  CONSTRAINT `fk_resultado_resposta` FOREIGN KEY (`id_resposta`) REFERENCES `respostas` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-05-12 14:41:08
---
+++ script.sql
@@ -0,0 +1,94 @@
+CREATE DATABASE feedback;
+
+USE feedback;
+
+CREATE TABLE pessoas (
+	id int primary key auto_increment,
+    nome varchar(45) not null,
+    sobrenome varchar(45) not null,
+    cpf varchar(11) not null,
+    data_nascimento DATE not null,
+    telefone varchar(18) not null,
+    login varchar(45) not null,
+    email varchar(45) not null,
+    senha varchar(20) not null,
+    id_endereco int not null
+);
+
+ALTER TABLE pessoas ADD CONSTRAINT fk_pessoas_endereco
+FOREIGN KEY (id_endereco) REFERENCES endereco (id);
+
+CREATE TABLE empresas (
+	id int primary key auto_increment,
+    razao_social varchar(80) not null,
+    nome_fantasia varchar(80) not null,
+    cnpj varchar(11) not null,
+    atividade varchar(45) not null,
+    email varchar(45) not null,
+    telefone varchar(18) not null,
+    login varchar(45) not null,
+    senha varchar(20) not null,
+    logo varchar(400) not null,
+    id_endereco int not null
+);
+
+ALTER TABLE empresas ADD CONSTRAINT fk_empresas_endereco
+FOREIGN KEY (id_endereco) REFERENCES endereco (id);
+
+
+CREATE TABLE endereco (
+	id int primary key auto_increment,
+    tipo varchar(30) not null,
+    logradouro varchar(120) not null,
+    numero int not null,
+    cep varchar(8) not null,
+    cidade varchar(45) not null,
+    estado varchar(2) not null
+);
+
+CREATE TABLE enquete (
+	id int primary key auto_increment,
+    numero int not null,
+    descricao varchar(120) not null,
+    id_empresa int not null
+);
+
+ALTER TABLE enquete ADD CONSTRAINT fk_enquete_empresa
+FOREIGN KEY (id_empresa) REFERENCES empresas (id);
+
+CREATE TABLE perguntas (
+	id int primary key auto_increment,
+    numero int not null,
+    descricao varchar(120) not null,
+    id_enquete int not null
+);
+
+ALTER TABLE pergunta ADD CONSTRAINT fk_pergunta_enquete
+FOREIGN KEY (id_enquete) REFERENCES enquete (id);
+
+CREATE TABLE respostas (
+	id int primary key auto_increment,
+    numero int not null,
+    descricao varchar(120) not null,
+    id_pergunta int not null
+);
+
+ALTER TABLE respostas ADD CONSTRAINT fk_resposta_pergunta
+FOREIGN KEY (id_pergunta) REFERENCES perguntas (id);
+
+CREATE TABLE resultados (
+	id int primary key not null,
+    id_resposta int not null,
+    id_pessoa int not null
+);
+
+ALTER TABLE resultados ADD CONSTRAINT fk_resultado_resposta
+FOREIGN KEY (id_resposta) REFERENCES respostas (id);
+
+ALTER TABLE resultados ADD CONSTRAINT fk_resultado_pessoa
+FOREIGN KEY (id_pessoa) REFERENCES pessoas (id);
+
+    
+
+
+    
