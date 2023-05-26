51673e90d172599896ff169e6f78003b0452dedd
David Marques <100491361+DavidMarqss@users.noreply.github.com>
2023-05-26 00:54:12 -0300

    up:inserts

---
+++ inserts.sql
@@ -0,0 +1,47 @@
+/*----Empresas----*/
+insert into empresas(razao_social, nome_fantasia, cnpj, atividade, email, telefone, login, senha, logo,id_endereco) values('Mercado&Cia','Mercadão','19728045000105','Comércio','mercadao@email.com','14995845756','mercadao_adm','775365124','img15423',1);
+insert into empresas(razao_social, nome_fantasia, cnpj, atividade, email, telefone, login, senha, logo,id_endereco) values('Advocacia Ltda','Escritorio Lima','60816992000151','Comércio','juridico@advocacialtda.com.br','14991178380','advocacia_adm','214207569','img45721',6);
+insert into empresas(razao_social, nome_fantasia, cnpj, atividade, email, telefone, login, senha, logo,id_endereco) values('Pães e Doces ME','Panificadora Super','52746775000104','Comércio','diretoria@paesedocesme.com.br','14997562320','doces_adm','662037365','img35847',9);
+insert into empresas(razao_social, nome_fantasia, cnpj, atividade, email, telefone, login, senha, logo,id_endereco) values('Informática ME','Casa do TI','38887778000137','Comércio','representantes@informaticame.com.br','14989848271','ti_adm','791172442','img96547',7);
+insert into empresas(razao_social, nome_fantasia, cnpj, atividade, email, telefone, login, senha, logo,id_endereco) values('Alimentos Ltda','Distribuidora FT','22049197000196','Comércio','faleconosco@alimentosltda.com.br','14983709058','alimento_adm','989063206','img21457',5);
+insert into empresas(razao_social, nome_fantasia, cnpj, atividade, email, telefone, login, senha, logo,id_endereco) values('Consultoria Financeira ME','Rende+','67823644000150','Comércio','pesquisa@consultoriafinanceirame.com.br','14982831257','rende_adm','393229975','img71425',8);
+insert into empresas(razao_social, nome_fantasia, cnpj, atividade, email, telefone, login, senha, logo,id_endereco) values('Pizzaria Delivery ME','Rei das pizzas','74891119000138','Comércio','tesouraria@pizzariadeliveryme.com.br','14999659683','reipizza_adm','407933886','img02145',2);
+insert into empresas(razao_social, nome_fantasia, cnpj, atividade, email, telefone, login, senha, logo,id_endereco) values('Farmácia Saúde', 'Saúde+', '56789012345678', 'Saúde', 'contato@saudeplus.com', '1434567890', 'saudeplus_adm','278439097','img74662',3);
+insert into empresas(razao_social, nome_fantasia, cnpj, atividade, email, telefone, login, senha, logo,id_endereco) values('Restaurante Saboroso', 'Sabor & Cia', '90123456789012', 'Comércio', 'contato@saboroso.com', '14987654321', 'saboroso_adm','634489496','img42142',10);
+insert into empresas(razao_social, nome_fantasia, cnpj, atividade, email, telefone, login, senha, logo,id_endereco) values('Supermercado ABC', 'Super ABC', '12345678901234', 'Comércio', 'contato@superabc.com', '149876543210', 'superabc_adm','778325539','img96534',4);
+
+/*----Endereços Empresas----*/
+insert into endereco(tipo,logradouro,numero,cep,cidade,estado) values('Comercial','Rua Jorge Streit','82','17605140','Tupã','SP');
+insert into endereco(tipo,logradouro,numero,cep,cidade,estado) values('Comercial','Rua João Capioto','257','17602040','Tupã','SP');
+insert into endereco(tipo,logradouro,numero,cep,cidade,estado) values('Comercial','Rua Alfredo Modelli','421','17603870','Tupã','SP');
+insert into endereco(tipo,logradouro,numero,cep,cidade,estado) values('Comercial','Rua Carlos Bueno Toledo','165','17580970','Pompéia','SP');
+insert into endereco(tipo,logradouro,numero,cep,cidade,estado) values('Comercial','Rua Francisco Marques Beato','60','17580971','Pompéia','SP');
+insert into endereco(tipo,logradouro,numero,cep,cidade,estado) values('Comercial','Rua Alagoas','1111','17730970','Parapuã','SP');
+insert into endereco(tipo,logradouro,numero,cep,cidade,estado) values('Comercial','Viela Cinco','450','19910243','Ourinhos','SP');
+insert into endereco(tipo,logradouro,numero,cep,cidade,estado) values('Comercial','Rua Henrique Chiarato','540','19912260','Ourinhos','SP');
+insert into endereco(tipo,logradouro,numero,cep,cidade,estado) values('Comercial','Viela Três','1054','19910239','Ourinhos','SP');
+insert into endereco(tipo,logradouro,numero,cep,cidade,estado) values('Comercial','Rua 8','19','13500144','Rio Claro','SP');
+
+/*----Endereços Pessoas----*/
+insert into endereco(tipo,logradouro,numero,cep,cidade,estado) values('Residencial','Rua Jorge Streit','18','17605140','Tupã','SP');
+insert into endereco(tipo,logradouro,numero,cep,cidade,estado) values('Residencial','Rua João Capioto','452','17602040','Tupã','SP');
+insert into endereco(tipo,logradouro,numero,cep,cidade,estado) values('Residencial','Rua Alfredo Modelli','71','17603870','Tupã','SP');
+insert into endereco(tipo,logradouro,numero,cep,cidade,estado) values('Residencial','Rua Carlos Bueno Toledo','645','17580970','Pompéia','SP');
+insert into endereco(tipo,logradouro,numero,cep,cidade,estado) values('Residencial','Rua Francisco Marques Beato','42','17580971','Pompéia','SP');
+insert into endereco(tipo,logradouro,numero,cep,cidade,estado) values('Residencial','Rua Alagoas','13','17730970','Parapuã','SP');
+insert into endereco(tipo,logradouro,numero,cep,cidade,estado) values('Residencial','Viela Cinco','63','19910243','Ourinhos','SP');
+insert into endereco(tipo,logradouro,numero,cep,cidade,estado) values('Residencial','Rua Henrique Chiarato','367','19912260','Ourinhos','SP');
+insert into endereco(tipo,logradouro,numero,cep,cidade,estado) values('Residencial','Viela Três','987','19910239','Ourinhos','SP');
+insert into endereco(tipo,logradouro,numero,cep,cidade,estado) values('Residencial','Rua 8','1457','13500144','Rio Claro','SP');
+
+/*----Pessoas----*/
+insert into pessoas(nome,sobrenome,cpf,data_nascimento,telefone,login,email,senha,id_endereco) values('Isabel','Freitas','02502503060','1990-03-05','14998693045','isafreitas','isafreitas@email.com','585426955',16);
+insert into pessoas(nome,sobrenome,cpf,data_nascimento,telefone,login,email,senha,id_endereco) values('João', 'Silva', '12345678901', '1985-07-15', '11987654321', 'joaosilva', 'joao.silva@email.com', '823688014', 11);
+insert into pessoas(nome,sobrenome,cpf,data_nascimento,telefone,login,email,senha,id_endereco) values('Maria', 'Souza', '98765432109', '1992-12-02', '21987654321', 'mariasouza', 'maria.souza@email.com', '085189870', 15);
+insert into pessoas(nome,sobrenome,cpf,data_nascimento,telefone,login,email,senha,id_endereco) values('Pedro', 'Tozo', '45678912304', '2004-01-30', '31987654321', 'pedrotozo', 'pedro.tozo@email.com', '563554300', 20);
+insert into pessoas(nome,sobrenome,cpf,data_nascimento,telefone,login,email,senha,id_endereco) values('Ana', 'Santos', '78912345607', '1995-09-10', '41987654321', 'anasantos', 'ana.santos@email.com', '227373596', 14);
+insert into pessoas(nome,sobrenome,cpf,data_nascimento,telefone,login,email,senha,id_endereco) values('Carlos', 'Oliveira', '34567891234', '1983-02-25', '51987654321', 'carlosoliveira', 'carlos.oliveira@email.com', '278127591', 20);
+insert into pessoas(nome,sobrenome,cpf,data_nascimento,telefone,login,email,senha,id_endereco) values('Fernanda', 'Ribeiro', '89012345678', '1990-11-08', '61987654321', 'fernandaribeiro', 'fernanda.ribeiro@email.com', '084290296', 12);
+insert into pessoas(nome,sobrenome,cpf,data_nascimento,telefone,login,email,senha,id_endereco) values('Ricardo', 'Almeida', '56789123450', '1987-06-18', '71987654321', 'ricardoalmeida', 'ricardo.almeida@email.com', '374679042', 14);
+insert into pessoas(nome,sobrenome,cpf,data_nascimento,telefone,login,email,senha,id_endereco) values('Juliana', 'Gomes', '90123456712', '1993-03-12', '81987654321', 'julianagomes', 'juliana.gomes@email.com', '737525773', 17);
+insert into pessoas(nome,sobrenome,cpf,data_nascimento,telefone,login,email,senha,id_endereco) values('Lucas', 'Martins', '23456789103', '1989-08-29', '91987654321', 'lucasmartins', 'lucas.martins@email.com', '137219658', 18);
