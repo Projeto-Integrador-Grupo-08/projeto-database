643cb3bb0b43d640a6e6d0af9ab360b1c8847fcc
David Marques <100491361+DavidMarqss@users.noreply.github.com>
2023-06-02 15:19:36 -0300

    Update inserts.sql

--- inserts.sql
+++ inserts.sql
@@ -45,3 +45,221 @@ insert into pessoas(nome,sobrenome,cpf,data_nascimento,telefone,login,email,senh
 insert into pessoas(nome,sobrenome,cpf,data_nascimento,telefone,login,email,senha,id_endereco) values('Ricardo', 'Almeida', '56789123450', '1987-06-18', '71987654321', 'ricardoalmeida', 'ricardo.almeida@email.com', '374679042', 14);
 insert into pessoas(nome,sobrenome,cpf,data_nascimento,telefone,login,email,senha,id_endereco) values('Juliana', 'Gomes', '90123456712', '1993-03-12', '81987654321', 'julianagomes', 'juliana.gomes@email.com', '737525773', 17);
 insert into pessoas(nome,sobrenome,cpf,data_nascimento,telefone,login,email,senha,id_endereco) values('Lucas', 'Martins', '23456789103', '1989-08-29', '91987654321', 'lucasmartins', 'lucas.martins@email.com', '137219658', 18);
+
+/*----Enquetes----*/
+INSERT INTO enquete(numero, descricao, id_empresa) VALUES(1,'Revisão dos produtos da empresa',1);
+INSERT INTO enquete (numero, descricao, id_empresa) VALUES (2, 'Avaliação da satisfação dos clientes com o atendimento pós-venda', 1);
+INSERT INTO enquete (numero, descricao, id_empresa) VALUES (3, 'Preferência dos clientes em relação aos produtos oferecidos', 1);
+INSERT INTO enquete (numero, descricao, id_empresa) VALUES (4, 'Opinião dos funcionários sobre o ambiente de trabalho', 4);
+INSERT INTO enquete (numero, descricao, id_empresa) VALUES (5, 'Feedback dos clientes sobre a qualidade dos serviços prestados', 4);
+INSERT INTO enquete (numero, descricao, id_empresa) VALUES (6, 'Percepção dos clientes em relação à marca da empresa', 5);
+INSERT INTO enquete (numero, descricao, id_empresa) VALUES (7, 'Opinião dos fornecedores sobre o processo de compras da empresa', 8);
+INSERT INTO enquete (numero, descricao, id_empresa) VALUES (8, 'Avaliação dos funcionários sobre os benefícios oferecidos pela empresa', 2);
+INSERT INTO enquete (numero, descricao, id_empresa) VALUES (9, 'Interesse dos clientes em novos produtos ou serviços', 2);
+INSERT INTO enquete (numero, descricao, id_empresa) VALUES (10, 'Feedback dos colaboradores sobre a eficácia da comunicação interna', 6);
+
+/*----Perguntas----*/
+INSERT INTO perguntas(numero, descricao, id_enquete) VALUES(1, 'Gosta dos produtos da empresa?',1);
+INSERT INTO perguntas(numero, descricao, id_enquete) VALUES(2, 'Gostaria de Mudanças nos Produtos?',1);
+
+INSERT INTO perguntas (numero, descricao, id_enquete) VALUES (1, 'Como você avalia o atendimento pós-venda da empresa?', 2);
+INSERT INTO perguntas (numero, descricao, id_enquete) VALUES (2, 'Você já teve algum problema não resolvido pelo atendimento pós-venda?', 2);
+
+INSERT INTO perguntas (numero, descricao, id_enquete) VALUES (1, 'Quais produtos você mais utiliza da empresa?', 3);
+INSERT INTO perguntas (numero, descricao, id_enquete) VALUES (2, 'Há algum produto que você acha que poderia ser melhorado?', 3);
+
+INSERT INTO perguntas (numero, descricao, id_enquete) VALUES (1, 'Como você descreveria o ambiente de trabalho na empresa?', 4);
+INSERT INTO perguntas (numero, descricao, id_enquete) VALUES (2, 'Você se sente motivado(a) no ambiente de trabalho?', 4);
+
+INSERT INTO perguntas (numero, descricao, id_enquete) VALUES (1, 'Qual é a sua opinião sobre a qualidade dos serviços prestados pela empresa?', 5);
+INSERT INTO perguntas (numero, descricao, id_enquete) VALUES (2, 'Você já teve alguma experiência negativa com os serviços prestados?', 5);
+
+INSERT INTO perguntas (numero, descricao, id_enquete) VALUES (1, 'Como você percebe a marca da empresa?', 6);
+INSERT INTO perguntas (numero, descricao, id_enquete) VALUES (2, 'Você recomendaria a marca da empresa a outras pessoas?', 6);
+
+INSERT INTO perguntas (numero, descricao, id_enquete) VALUES (1, 'Qual é a sua opinião sobre o processo de compras da empresa?', 7);
+INSERT INTO perguntas (numero, descricao, id_enquete) VALUES (2, 'Você já teve algum problema na relação com os fornecedores da empresa?', 7);
+
+INSERT INTO perguntas (numero, descricao, id_enquete) VALUES (1, 'Quais benefícios oferecidos pela empresa você mais valoriza?', 8);
+INSERT INTO perguntas (numero, descricao, id_enquete) VALUES (2, 'Você acredita que os benefícios oferecidos são adequados?', 8);
+
+INSERT INTO perguntas (numero, descricao, id_enquete) VALUES (1, 'Quais novos produtos ou serviços você gostaria de ver da empresa?', 9);
+INSERT INTO perguntas (numero, descricao, id_enquete) VALUES (2, 'Você está aberto(a) a experimentar novos produtos ou serviços da empresa?', 9);
+
+INSERT INTO perguntas (numero, descricao, id_enquete) VALUES (1, 'Como você avalia a comunicação interna na empresa?', 10);
+INSERT INTO perguntas (numero, descricao, id_enquete) VALUES (2, 'Você se sente bem informado(a) sobre os assuntos relacionados à empresa?', 10);
+
+/*----Respostas----*/
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (1, 'Sim', 1);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (2, 'Não', 1);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (3, 'Indiferente', 1);
+
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (1, 'Adoraria', 2);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (2, 'Indiferente', 2);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (3, 'Odiaria', 2);
+
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (1, 'Produto A', 3);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (2, 'Produto B', 3);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (3, 'Produto C', 3);
+
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (1, 'Ótimo', 4);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (2, 'Bom', 4);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (3, 'Regular', 4);
+
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (1, 'Muito satisfeito(a)', 5);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (2, 'Satisfeito(a)', 5);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (3, 'Insatisfeito(a)', 5);
+
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (1, 'Muito positiva', 6);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (2, 'Neutra', 6);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (3, 'Negativa', 6);
+
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (1, 'Muito satisfeito(a)', 7);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (2, 'Satisfeito(a)', 7);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (3, 'Insatisfeito(a)', 7);
+
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (1, 'Sim', 8);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (2, 'Não', 8);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (3, 'Não tenho certeza', 8);
+
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (1, 'Novo Produto A', 9);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (2, 'Novo Produto B', 9);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (3, 'Novo Produto C', 9);
+
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (1, 'Muito eficaz', 10);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (2, 'Razoavelmente eficaz', 10);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (3, 'Ineficaz', 10);
+
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (1, 'Sim', 11);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (2, 'Não', 11);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (3, 'Não aplicável', 11);
+
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (1, 'Muito pontual', 12);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (2, 'Pontual na maioria das vezes', 12);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (3, 'Frequentemente atrasado', 12);
+
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (1, 'Muito eficiente', 13);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (2, 'Eficiente na maioria das vezes', 13);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (3, 'Frequentemente com dificuldades', 13);
+
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (1, 'Sim', 14);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (2, 'Não', 14);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (3, 'Não tenho certeza', 14);
+
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (1, 'Muito satisfeito(a)', 15);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (2, 'Satisfeito(a)', 15);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (3, 'Insatisfeito(a)', 15);
+
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (1, 'Sim', 16);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (2, 'Não', 16);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (3, 'Não tenho certeza', 16);
+
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (1, 'Sim', 17);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (2, 'Não', 17);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (3, 'Não aplicável', 17);
+
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (1, 'Sim', 18);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (2, 'Não', 18);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (3, 'Não aplicável', 18);
+
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (1, 'Sim', 11);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (2, 'Não', 11);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (3, 'Não aplicável', 11);
+
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (1, 'Muito pontual', 12);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (2, 'Pontual na maioria das vezes', 12);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (3, 'Frequentemente atrasado', 12);
+
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (1, 'Muito eficiente', 13);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (2, 'Eficiente na maioria das vezes', 13);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (3, 'Frequentemente com dificuldades', 13);
+
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (1, 'Sim', 14);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (2, 'Não', 14);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (3, 'Não tenho certeza', 14);
+
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (1, 'Muito satisfeito(a)', 15);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (2, 'Satisfeito(a)', 15);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (3, 'Insatisfeito(a)', 15);
+
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (1, 'Sim', 16);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (2, 'Não', 16);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (3, 'Não tenho certeza', 16);
+
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (1, 'Sim', 17);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (2, 'Não', 17);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (3, 'Não aplicável', 17);
+
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (1, 'Sim', 18);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (2, 'Não', 18);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (3, 'Não aplicável', 18);
+
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (1, 'Sim', 19);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (2, 'Não', 19);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (3, 'Não tenho certeza', 19);
+
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (1, 'Muito útil', 20);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (2, 'Útil na maioria das vezes', 20);
+INSERT INTO respostas (numero, descricao, id_pergunta) VALUES (3, 'Pouco útil', 20);
+
+/*----Resultados----*/
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (1, 1);
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (1, 2);  
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (1, 9);  
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (6, 8);  
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (4, 8);  
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (5, 7);  
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (9, 7);  
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (7, 2);  
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (8, 8);  
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (12, 2); 
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (12, 3); 
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (11, 3); 
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (15, 8); 
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (13, 9); 
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (15, 8); 
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (16, 10);
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (17, 10);
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (16, 3); 
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (21, 8); 
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (20, 5); 
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (19, 7); 
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (23, 5); 
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (24, 9); 
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (22, 9); 
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (26, 5); 
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (27, 6); 
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (27, 7); 
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (30, 5); 
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (28, 3); 
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (28, 5); 
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (33, 1); 
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (33, 3); 
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (32, 4);
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (35, 1);
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (36, 7);
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (35, 6);
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (39, 7);
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (37, 8);
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (37, 5);
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (42, 7);
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (41, 6);
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (40, 5);
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (43, 10);
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (45, 8);
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (44, 7);
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (47, 10);
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (46, 1);
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (48, 10);
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (50, 5);
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (51, 8);
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (49, 6);
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (52, 2);
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (54, 4);
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (53, 10);
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (56, 9);
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (56, 1);
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (55, 7);
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (60, 4);
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (60, 8);
+INSERT INTO resultados (id_resposta, id_pessoa) VALUES (60, 4);
